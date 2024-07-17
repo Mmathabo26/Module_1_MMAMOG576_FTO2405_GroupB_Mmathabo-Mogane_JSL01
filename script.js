978ca073015efa78534d78bfd4ccc448debaee1e
Mmathabo Mogane <mmathaboappreciate@gmail.com>
2024-07-18 01:56:16 +0200

    correct conditions

--- script.js
+++ script.js
@@ -2,15 +2,18 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
 
-    
+    s1= "pet_2015Forrest";
+    let result = s1.startsWith("pet_");       
 
-    let result = 'pet_'; // Placeholder for validation result
+    // let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
             
-        if (input.indexOf("pet_") === 0) {
-            console.log("Valid syntax")
+        if (input.startsWith("pet_") && input.endsWith("/^[A-Az-z0-9\s]+$/")) {
+            console.log("Valid Syntax")
+        } else {
+            console.log("Invalid Syntax")
         }
 
             document.getElementById('result').innerText = result;
