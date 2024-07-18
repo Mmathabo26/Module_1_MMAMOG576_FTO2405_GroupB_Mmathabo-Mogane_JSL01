91d7439ff7b0e740df57c8865e365f0a77c3cdae
Mmathabo Mogane <mmathaboappreciate@gmail.com>
2024-07-18 03:45:49 +0200

    validate condition

--- script.js
+++ script.js
@@ -2,16 +2,15 @@ function validateSyntax() {
     let input = document.getElementById('petInput').value;
     // Validation logic goes here
 
-    s1= "pet_2015Forrest";
-    let result = s1.startsWith("pet_");      
-    let result2 = s1.endsWith("/^[A-Az-z0-9\s]+$/"); 
+    let String = "pet_2015Forrest";
+    let result = s1.startsWith("pet_") 
 
     // let result = ''; // Placeholder for validation result
 
     // TODO: Write your validation logic here
         // Check if input starts with 'pet_' and followed by alphanumeric characters
             
-        if (input.startsWith("pet_") && input.endsWith("/^[A-Az-z0-9\s]+$/")) {
+        if (input.startsWith("pet_") && input.endsWith("^[a-zA-Z0-9]+$") ) {
             console.log("Valid Syntax")
         } else {
             console.log("Invalid Syntax")
